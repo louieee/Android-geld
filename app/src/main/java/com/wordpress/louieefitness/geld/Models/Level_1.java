9beb3e35715d9ba6205564367e84282b3ef9128c
Louis <louis.paul9095@gmail.com>
2019-01-17 16:07:32 +0100

    added database persistence and a method that increments no_received to all activities

--- app/src/main/java/com/wordpress/louieefitness/geld/Models/Level_1.java
+++ app/src/main/java/com/wordpress/louieefitness/geld/Models/Level_1.java
@@ -1,7 +1,18 @@
 package com.wordpress.louieefitness.geld.Models;
 
+import android.util.Log;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.MutableData;
+import com.google.firebase.database.Transaction;
+
 public class Level_1 {
     private String username, no_received, color;
+    final public String  ref = "Level_1";
+    private Level_1 my_level;
 
     public Level_1(){
 
@@ -36,4 +47,31 @@ public class Level_1 {
     public void setColor(String color) {
         this.color = color;
     }
+    public void increment_no_received() {
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference(ref);
+        myRef.runTransaction(new Transaction.Handler() {
+            @Override
+            public Transaction.Result doTransaction(MutableData mutableData) {
+                my_level = mutableData.getValue(Level_1.class);
+                if (my_level == null) {
+                    return Transaction.success(mutableData);
+                }
+                int no_received = Integer.parseInt(my_level.getNo_received());
+                no_received += 1;
+                my_level.setNo_received(String.valueOf(no_received));
+                // Set value and report transaction success
+                mutableData.setValue(my_level);
+                return Transaction.success(mutableData);
+            }
+
+            @Override
+            public void onComplete(DatabaseError databaseError, boolean b,
+                                   DataSnapshot dataSnapshot) {
+                // Transaction completed
+                Log.d("Message: ", "postTransaction:onComplete:" + databaseError);
+            }
+        });
+    }
+
 }
